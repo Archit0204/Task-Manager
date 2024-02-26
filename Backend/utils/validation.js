355d430e71f696efecced1502376b07dfa0e77ba
Archit0204 <amittal0204@gmail.com>
2024-02-26 19:27:29 +0530

    validation utility

---
+++ Backend/utils/validation.js
@@ -0,0 +1,19 @@
+const zod = require("zod");
+
+exports.validation = (field, type) => {
+
+    const emailSchema = zod.string().email();
+    const passwordSchema = zod.string().min(8);
+    const nameSchema = zod.string();
+
+    if (type === "email") {
+        return emailSchema.safeParse(field).success;
+    }
+    else if (type === "pass") {
+        return passwordSchema.safeParse(field).success;
+    }
+    else if (type === "name") {
+        return nameSchema.safeParse(field).success;
+    }
+
+}
