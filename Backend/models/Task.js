238071f1f70d43e777a8d7424aab1f1049128c34
Archit0204 <amittal0204@gmail.com>
2024-02-29 20:01:50 +0530

    Change in Status

--- Backend/models/Task.js
+++ Backend/models/Task.js
@@ -14,6 +14,7 @@ const taskSchema = new mongoose.Schema({
     status: {
         type: String,
         enum: ["Pending", "Completed"],
+        default: "Pending"
     },
     createdAt: {
         type: Date,
