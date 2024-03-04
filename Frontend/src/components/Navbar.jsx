ad49023e737523665b312f06367e2cb94df0f3bc
Archit0204 <amittal0204@gmail.com>
2024-03-04 19:19:42 +0530

    updated nav

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -11,13 +11,19 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
         localStorage.removeItem("token");
         setIsLoggedIn(false);
         toast.success("Logged Out");
+        navigate("/login");
+    }
+
+    function homeHandler() {
+        localStorage.removeItem("token");
+        setIsLoggedIn(false);
         navigate("/");
     }
 
     return (
         <div className="absolute flex flex-row items-center justify-center w-screen h-14 bg-myviolet-100">
             <div className="w-[90%] max-w-[1250px] flex justify-between">
-                <p className="font-bold text-white text-3xl">FocadoX</p>
+                <p onClick={homeHandler} className="font-bold text-white text-3xl cursor-pointer">FocadoX</p>
                 <div className="flex flex-row font-semibold w-96 justify-around items-center text-white">
                     <div className="flex justify-between gap-x-3">
                         <Link to="/about">
@@ -28,23 +34,6 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
                         </Link>
                     </div>
                     <div>
-                        {/* {isLoggedIn ? (
-                            <div className="flex justify-between gap-x-3">
-                                <Link to="/dashboard">
-                                    <p>Dashboard</p>
-                                </Link>
-                                <button onClick={logoutHandler}>Logout</button>
-                            </div>
-                        ) : (
-                            <div className="flex justify-between gap-x-3">
-                                <Link to="/login">
-                                    <p>Login</p>
-                                </Link>
-                                <Link to="/signup">
-                                    <p>Signup</p>
-                                </Link>
-                            </div>
-                        )} */}
                         {
                             isLoggedIn &&
                             <div className="flex justify-between gap-x-3">
