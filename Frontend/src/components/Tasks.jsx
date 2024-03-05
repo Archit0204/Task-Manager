d956675206af46aaab7d0439d8c510f83d7e313d
Archit0204 <amittal0204@gmail.com>
2024-03-06 02:06:29 +0530

    filter

--- Frontend/src/components/Tasks.jsx
+++ Frontend/src/components/Tasks.jsx
@@ -3,43 +3,42 @@ import Task from "./Task";
 import { GoSearch } from "react-icons/go";
 import toast from "react-hot-toast";
 
-function Tasks({ tasks, setTasks }) {
+function Tasks({ tasks, setTasks, searchHandler, filterHandler, filterChangeHandler, removeFilterHandler, search, filter, setSearch }) {
 
 
-    const [search, setSearch] = useState("");
 
-    async function searchHandler() {
-        try {
-            const token = localStorage.getItem("token");
-            const response = await fetch(`http://localhost:4000/api/v1/task/search/${search}`, {
-                method: 'GET',
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                }
-            });
-            const data = await response.json();
-            // console.log(data);
-            if (data.success) {
-                setTasks(data.user.tasks);
-            }
-            // console.log(tasks);
-        }
-        catch (err) {
-            toast.error("Network Error");
-        }
-    }
 
     return (
-        <div className="flex flex-col justify-center gap-y-4 border-2 border-myviolet-100 bg-myviolet-100 p-8 mb-10 rounded-xl">
+        <div className="flex flex-col justify-center gap-y-6 border-2 border-myviolet-100 bg-myviolet-100 p-8 mb-10 rounded-xl">
+            <div className="flex flex-col gap-y-5">
+                <div className="relative flex items-center min-w-[250px] w-[30%]">
+                    <input className="w-full border border-myviolet-100 focus:outline-none pl-3 pr-12 py-2 rounded-lg" type="text" placeholder="Search" value={search} onChange={(event) => setSearch(event.target.value)} />
+                    <button onClick={searchHandler} className="absolute right-5"><GoSearch /></button>
+                </div>
+
+                <div className="flex flex-col gap-y-5 min-w-[200px] w-[20%]">
+                    <label className="flex gap-x-5 items-center">
+                        <p className="font-medium text-base mb-2 text-white">Sort by Priority:</p>
+                        <select onChange={filterChangeHandler} className="border focus:outline-none border-myviolet-100 rounded-md py-1 px-2" name="priority" value={filter}>
+                            <option value="Low">Low</option>
+                            <option value="Medium">Medium</option>
+                            <option value="High">High</option>
+                        </select>
+                    </label>
+                </div>
+
+                <div className="flex gap-x-5">
+                    <button onClick={filterHandler} className="text-myviolet-100 bg-white rounded-md px-2 py-1 font-semibold">Filter Results</button>
+                    <button onClick={removeFilterHandler} className="text-myviolet-100 bg-white rounded-md px-2 py-1 font-semibold">Remove Filters</button>
+                </div>
+            </div>
+
             {
                 tasks.length === 0 ? (
                     <div className="text-white mx-auto text-2xl font-semibold">No Tasks Found</div>
                 ) : (
                     <div className="flex flex-col justify-center gap-y-4">
-                        <div className="relative flex items-center min-w-[250px] w-[30%]">
-                            <input className="w-full border border-myviolet-100 focus:outline-none pl-3 pr-12 py-2 rounded-lg" type="text" placeholder="Search" value={search} onChange={(event) => setSearch(event.target.value)} />
-                            <button onClick={searchHandler} className="absolute right-5"><GoSearch /></button>
-                        </div>
+                        <h1 className="text-white text-4xl font-semibold">Your Tasks</h1>
                         {
                             tasks.map((task) => {
                                 return <Task task={task} id={task._id} setTasks={setTasks} />;
--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -14,6 +14,86 @@ function Dashboard() {
         navigate("/create");
     }
 
+    const [search, setSearch] = useState("");
+    const [filter, setFilter] = useState("High");
+
+    async function searchHandler() {
+        setLoading(true);
+        try {
+            const token = localStorage.getItem("token");
+            const response = await fetch(`http://localhost:4000/api/v1/task/search/${search}`, {
+                method: 'GET',
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+            const data = await response.json();
+            // console.log(data);
+            if (data.success) {
+                setTasks(data.user.tasks);
+                // console.log(tasks);
+                setSearch("");
+            }
+        }
+        catch (err) {
+            toast.error("Network Error");
+        }
+        setLoading(false);
+    }
+
+    async function removeFilterHandler() {
+        setLoading(true);
+        try {
+            const token = localStorage.getItem("token")
+            const response = await fetch("http://localhost:4000/api/v1/task/showTasks", {
+                method: 'GET',
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+            const data = await response.json();
+            // console.log(data);
+
+            if (data.success) {
+                setTasks(data.user.tasks);
+            }
+        }
+        catch (err) {
+            toast.error("Network Problem");
+        }
+        setLoading(false);
+    }
+
+    async function filterChangeHandler(event) {
+
+        setFilter(event.target.value);
+        // console.log(filter);
+    }
+
+    async function filterHandler() {
+        setLoading(true);
+        try {
+            const token = localStorage.getItem("token");
+            const response = await fetch(`http://localhost:4000/api/v1/task/filter/${filter}`, {
+                method: 'GET',
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                }
+            });
+            const data = await response.json();
+            // console.log(data);
+
+            if (data.success) {
+                setTasks(data.user.tasks);
+            }
+        }
+        catch (err) {
+            toast.error("Network Error");
+            console.log(err.message);
+        }
+        setLoading(false);
+    }
+
     useEffect(() => {
 
         async function fetchData() {
@@ -48,7 +128,7 @@ function Dashboard() {
                 <button className="text-white text-lg font-semibold bg-myviolet-100 px-3 py-1 rounded-md" onClick={clickHandler}>Add a New Task</button>
                 <div className="w-full mx-auto">
                     {
-                        loading ? (<Spinner />) : (<Tasks tasks={tasks} setTasks={setTasks}/>)
+                        loading ? (<Spinner />) : (<Tasks tasks={tasks} setTasks={setTasks} searchHandler={searchHandler} filterHandler={filterHandler} filterChangeHandler={filterChangeHandler} removeFilterHandler={removeFilterHandler} search={search} filter={filter} setSearch={setSearch}/>)
                     }
                 </div>
             </div>
